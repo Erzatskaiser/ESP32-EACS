6eec74f843c983148f906f644316a75a9d5a1f79
Emmanuel Butsana <ebutsana@gmail.com>
2024-12-24 15:27:04 +0400

    Updated RGB class

--- eacs_software/main/header/rgb.hpp
+++ eacs_software/main/header/rgb.hpp
@@ -2,18 +2,22 @@
 
 // Header files
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 
 // RGB LED Class
 class RGB_LED {
  public:
   RGB_LED(gpio_num_t R, gpio_num_t G, gpio_num_t B);
-  void setColor(int8_t R, int8_t G, int8_t B);
-  void setColor(int8_t* colors);
-  int8_t* getColor();
+  void setColor(uint8_t* colors);
+  void setRed(uint8_t R);
+  void setGreen(uint8_t G);
+  void setBlue(uint8_t B);
+  uint8_t* getColor();
   void off();
   void on();
 
  private:
-  int8_t colors[3]{0, 0, 0};
+  uint8_t colors[3]{0, 0, 0};
   gpio_num_t pinR, pinG, pinB;
+  void updateColor(uint8_t R, uint8_t G, uint8_t B);
 };
--- eacs_software/main/implement/rgb.cpp
+++ eacs_software/main/implement/rgb.cpp
@@ -1,5 +1,10 @@
-// Header file
+﻿// Header file
 #include "../header/rgb.hpp"
+
+#include <cstdint>
+
+#include "driver/ledc.h"
+#include "hal/ledc_types.h"
 
 // RGB LED Class constructor
 RGB_LED::RGB_LED(gpio_num_t R, gpio_num_t G, gpio_num_t B) {
@@ -9,25 +14,100 @@ RGB_LED::RGB_LED(gpio_num_t R, gpio_num_t G, gpio_num_t B) {
   pinB = B;
 
   // Configure the pins
-  gpio_config_t configuration{(1ULL << R) | (1ULL << G) | (1ULL << B),
-                              GPIO_MODE_OUTPUT, GPIO_PULLUP_DISABLE,
-                              GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE};
+  const gpio_config_t gpio_conf{(1ULL << R) | (1ULL << G) | (1ULL << B),
+                                GPIO_MODE_OUTPUT, GPIO_PULLUP_DISABLE,
+                                GPIO_PULLDOWN_DISABLE, GPIO_INTR_DISABLE};
+  gpio_config(&gpio_conf);
+
+  // PWM timer configuration
+  const ledc_timer_config_t timer_conf{LEDC_HIGH_SPEED_MODE, LEDC_TIMER_8_BIT,
+                                       LEDC_TIMER_0, 5000};
+  ledc_timer_config(&timer_conf);
+
+  // PWM red channel configuration
+  const ledc_channel_config_t red_channel_conf{R,
+                                               LEDC_HIGH_SPEED_MODE,
+                                               LEDC_CHANNEL_0,
+                                               LEDC_INTR_DISABLE,
+                                               LEDC_TIMER_0,
+                                               static_cast<uint32_t>(colors[0]),
+                                               0,
+                                               {0}};
+  ledc_channel_config(&red_channel_conf);
+
+  // PWM green channel configuration
+  const ledc_channel_config_t green_channel_conf{
+      G,
+      LEDC_HIGH_SPEED_MODE,
+      LEDC_CHANNEL_1,
+      LEDC_INTR_DISABLE,
+      LEDC_TIMER_0,
+      static_cast<uint32_t>(colors[1]),
+      0,
+      {0}};
+  ledc_channel_config(&green_channel_conf);
+
+  // PWM blue channel configuration
+  const ledc_channel_config_t blue_channel_conf{
+      B,
+      LEDC_HIGH_SPEED_MODE,
+      LEDC_CHANNEL_2,
+      LEDC_INTR_DISABLE,
+      LEDC_TIMER_0,
+      static_cast<uint32_t>(colors[2]),
+      0,
+      {0}};
+  ledc_channel_config(&blue_channel_conf);
 }
 
-// setColor: (int8_t, int8_t, int8_t) --> (none)
+// setColor: (uint8_t, uint8_t, uint8_t) --> (none)
 // Sets the color of the LED
-void RGB_LED::setColor(int8_t R, int8_t G, int8_t B) { /* Code goes here */ }
+void RGB_LED::updateColor(uint8_t R, uint8_t G, uint8_t B) {
+  ledc_set_duty_and_update(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0,
+                           static_cast<uint32_t>(R), 0);
+  ledc_set_duty_and_update(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1,
+                           static_cast<uint32_t>(G), 0);
+  ledc_set_duty_and_update(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1,
+                           static_cast<uint32_t>(B), 0);
+}
 
-// setColor: (init8_t *) --> (none)
-// Sets the color of the LED
-void RGB_LED::setColor(int8_t* colors) { /* Code goes here */ }
+// setColor: (uint8_t*) --> (none)
+// Sets the color of the LED, updates state
+void RGB_LED::setColor(uint8_t* color) {
+  for (size_t i = 0; i < 3; i++) {
+    colors[i] = *color;
+    color++;
+  }
+  updateColor(colors[0], colors[1], colors[2]);
+}
 
-// getColor: (none) --> (int8_t*)
+// setRed: (uint8_t) --> (none)
+// Adjust the red value of the LED, updates state
+void RGB_LED::setRed(uint8_t R) {
+  colors[0] = R;
+  setColor(colors);
+}
+
+// setGreen: (uint8_t) --> (none)
+// Adjust the green value of the LED, updates state
+void RGB_LED::setGreen(uint8_t G) {
+  colors[1] = G;
+  setColor(colors);
+}
+
+// setBlue: (uint8_t) --> (none)
+// Adjust the blue value of the LED, updates state
+void RGB_LED::setBlue(uint8_t B) {
+  colors[2] = B;
+  setColor(colors);
+}
+
+// getColor: (none) --> (uint8_t*)
 // Returns a pointer to the array of current values
-int8_t* RGB_LED::getColor() { return colors; }
+uint8_t* RGB_LED::getColor() { return colors; }
 
 // off: (none) --> (none)
-// Turns of the RGB LED
+// Turns off the LED, updates state
 void RGB_LED::off() {
   for (size_t i = 0; i < 3; i++) {
     colors[i] = 0;
@@ -36,10 +116,10 @@ void RGB_LED::off() {
 }
 
 // on: (none) --> (none)
-// Turns on the RGB LED (White)
+// Sets the LED to white, updates state
 void RGB_LED::on() {
   for (size_t i = 0; i < 3; i++) {
-    colors[i] = 1;
+    colors[i] = 255;
   }
   setColor(colors);
 }
